a536602454e11b1fc8142c432fbc315dfee3221c
SpencerSoodek <spencersoodek@gmail.com>
2025-02-15 15:54:24 -0600

    Fix resolve dirname error and frontend dependencies

--- backend/server.js
+++ backend/server.js
@@ -39,7 +39,7 @@ app.use("/api/posts", postRoutes);
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.resolve(__dirname, "../frontend/dist")));
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve)(__dirname, "../frontend/dist/index.html");
+        res.sendFile(path.resolve((__dirname, "../frontend/dist/index.html")));
     });
 }
 
--- frontend/src/components/PrivatePost.jsx
+++ frontend/src/components/PrivatePost.jsx
@@ -1,5 +1,5 @@
 const PrivatePost = () => {
-    <div className="max-w-lg mx-auto border border-t-0border-neutral-content p-3 bg-base-100 mx-auto text-left pt-5 pb-5">
+    <div className="max-w-lg mx-auto border border-t-0border-neutral-content p-3 bg-base-100 text-left pt-5 pb-5">
         This post belongs to a private user.
     </div>
     
