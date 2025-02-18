f718c184cb3a7f3032e03ba788cfc9e98c8bfe4e
SpencerSoodek <spencersoodek@gmail.com>
2025-02-17 18:42:11 -0600

    Edited deploy action, use path resolve instead of join

--- backend/server.js
+++ backend/server.js
@@ -37,9 +37,9 @@ app.use("/api/followRequests", followRequestRoutes);
 app.use("/api/posts", postRoutes);
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "..", "frontend", "dist")));
+    app.use(express.static(path.resolve(__dirname, "..", "frontend", "dist")));
     app.get("*", (req, res) => {
-        res.sendFile(path.join((__dirname, "..", "frontend", "dist", "index.html")));
+        res.sendFile(path.resolve((__dirname, "..", "frontend", "dist", "index.html")));
     });
 }
 
